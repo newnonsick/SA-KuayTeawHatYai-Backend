60e53023bad611c8800840e2014c7f3859e5ca89
newnonsick <thitivath.m@ku.th>
2024-10-28 20:29:29 +0700

    test cicd

--- app/routes/index.py
+++ app/routes/index.py
@@ -4,4 +4,4 @@ index_blueprint = Blueprint('index', __name__)
 
 @index_blueprint.route('/', methods=['GET'])
 def index():
-    return jsonify({"message": "Hello, World!! I Pro kak"})
+    return jsonify({"message": "Hello, World!! I Pro kak Gu Good!"})
