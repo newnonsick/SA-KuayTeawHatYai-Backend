a64f0ad3cd44328968589f553bf2dc7b98783aaa
newnonsick <thitivath.m@ku.th>
2024-12-05 15:02:18 +0700

    refactor: enhance CORS configuration and improve error handling in the application

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,32 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from flask_socketio import SocketIO
 from werkzeug.exceptions import HTTPException
+
 from app.utils import verify_token
+
 from .config import Config
 from .db import init_db
-from flask_cors import CORS
 
-socketio = SocketIO(cors_allowed_origins="*")
+socketio = SocketIO(
+    cors_allowed_origins=[
+        "https://kuayteawhatyai.vercel.app",
+        "http://localhost",
+        "http://127.0.0.1",
+    ]
+)
+
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
+    CORS(
+        app,
+        origins=[
+            "https://kuayteawhatyai.vercel.app",
+            "http://localhost",
+            "http://127.0.0.1",
+        ],
+    )
 
     app.config.from_object(Config)
 
@@ -24,30 +40,37 @@ def create_app():
 
     # Register routes
     from .routes.index import index_blueprint
+
     app.register_blueprint(index_blueprint)
 
     from .routes.menus import menus_blueprint
+
     app.register_blueprint(menus_blueprint)
 
     from .routes.ingredients import ingredients_blueprint
+
     app.register_blueprint(ingredients_blueprint)
 
     from .routes.tables import tables_blueprint
+
     app.register_blueprint(tables_blueprint)
 
     from .routes.orders import orders_blueprint
+
     app.register_blueprint(orders_blueprint)
 
     from .routes.search import search_blueprint
+
     app.register_blueprint(search_blueprint)
 
     from .routes.income import income_blueprint
+
     app.register_blueprint(income_blueprint)
 
     @app.before_request
     def authenticate_request():
         """ตรวจสอบ Token ก่อนดำเนินการทุก Request"""
-        exempt_routes = ['/']
+        exempt_routes = ["/"]
 
         if request.path in exempt_routes:
             return
@@ -59,28 +82,35 @@ def create_app():
 
         if not verify_token(token):
             raise ValueError("Invalid or expired token")
-        
+
     @app.errorhandler(HTTPException)
     def handle_http_exception(error):
         response = error.get_response()
-        response.data = jsonify({
-            "code": error.code,
-            "name": error.name,
-            "description": error.description,
-        }).get_data(as_text=True)
+        response.data = jsonify(
+            {
+                "code": error.code,
+                "name": error.name,
+                "description": error.description,
+            }
+        ).get_data(as_text=True)
         response.content_type = "application/json"
         return response
 
     @app.errorhandler(Exception)
     def handle_exception(error):
         print(f"An error occurred: {str(error)}")
-        
+
         if isinstance(error, ValueError):
-            return jsonify({
-                "code": "Bad Request", "message": str(error)}), 400
-        
-        return jsonify({
-            "code": "Internal Server Error", "message": "An error occurred. Please try again later."
-        }), 500
+            return jsonify({"code": "Bad Request", "message": str(error)}), 400
+
+        return (
+            jsonify(
+                {
+                    "code": "Internal Server Error",
+                    "message": "An error occurred. Please try again later.",
+                }
+            ),
+            500,
+        )
 
     return app
